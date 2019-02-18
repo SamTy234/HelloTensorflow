6e6d3ece1f7188df0a612cc7181839b7a303c386
Samuel Gasper <1516828@my.brunel.ac.uk>
2019-02-18 22:07:10 +0000

    Cooked up a very roundabout way of actually outputting a .txt file that exists... Still works regardless

--- Barebonestest.py
+++ Barebonestest.py
@@ -17,11 +17,15 @@ def browse_file():
                                           filetypes= (("Audio files", "*.wav"),("All files", "*.*")))
     print(filename)
 
-def save_file():
-    global filesave
-    filesave = filedialog.asksaveasfilename(initialdir = "/home/Documents", title = "Save your text file",
+def save_filename():
+    global filesavename
+    filesavename = filedialog.asksaveasfilename(initialdir = "/home/Documents", title = "Save your text file",
                                           filetypes=(("Text files", "*.txt"),("All files", "*.")))
+    print(filesavename)
 
+def save_the_actual_file():
+    textfilename = input("Enter file name: ")
+    open(textfilename + ".txt", "wt")
 
 def print_name():
     print("This is a test to check that the button works")
@@ -30,7 +34,7 @@ menubar.add_cascade(label="File", menu=subMenu)
 subMenu.add_command(label="Open", command=browse_file)
 subMenu.add_command(label="Exit", command=root.destroy)
 
-outputTextButton = Button(root, text ="Get your text file", command= lambda : save_file())
+outputTextButton = Button(root, text ="Get your text file", command= lambda : save_the_actual_file())
 outputTextButton.pack()
 
 root.mainloop()
