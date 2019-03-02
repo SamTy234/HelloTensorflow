16a311b8870360e7ce2cc7c3a169ff949cba4107
Samuel Gasper <1516828@my.brunel.ac.uk>
2019-03-02 17:36:29 +0000

    Got matplotlib to work. Need to test it on spyder for it to work properly

--- Wavexperiment.py
+++ Wavexperiment.py
@@ -1,12 +1,12 @@
 import wave
-import matplotplib.pyplot as plt
+import matplotlib.pyplot as plt
 from scipy.io import wavfile
 
-x = wave.open("easy-song-1.wav", 'rb')
 
 samplerate, data = wavfile.read("easy-song-1.wav")
+x = wave.open("easy-song-1.wav", 'rb')
 
-allparams =x.getparams()
+allparams = x.getparams()
 readframes = [x.readframes(1), x.readframes(2), x.readframes(3), x.readframes(4), x.readframes(5)]
 
 # Stack overflow this later
@@ -15,6 +15,9 @@ bitrate = str((x.getframerate() * x.getsampwidth() * 8 * x.getnchannels()) / 100
 print(bitrate)
 
 plt.plot(data)
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.title("Test title")
 
 
 print("All params: " + str(allparams))
