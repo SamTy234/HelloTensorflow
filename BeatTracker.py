b8819511ecc22a84f818a70a0e212a992b8cff88
Samuel Gasper <1516828@my.brunel.ac.uk>
2019-03-17 03:14:41 +0000

    Added user input for BPM(needed to add to the start_bpm parameter in the beat.beat_track method. Also changed how BPM is calculated when printing to C1 page. ( tempo *2)

--- BeatTracker.py
+++ BeatTracker.py
@@ -11,24 +11,26 @@ x_coords = round(random.uniform(0.0, 1.0), 6)
 
 y, sr = librosa.load("easy-song-1.wav")
 
+
+BPM = input("Enter BPM: ")
+offset = input("Enter offset: ")
+
 # Beat tracker
-tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
-print(tempo)
-print(beats[:5])
+tempo, beats = librosa.beat.beat_track(y=y, sr=sr, start_bpm=BPM)
+
 
 #Convert beattracker frames to seconds
-beat_times = librosa.frames_to_time(beats, sr = sr)
+beat_times = librosa.frames_to_time(beats, sr=sr)
 beat_list = list(enumerate(beat_times))
 
 # for count, item in beat_list:
 #     print(count, round(item, 6))
-offset = input("Enter offset: ")
 
 
 with open("testfile.txt", "w+") as file:
     file.write("VERSION 2\n")
-    file.write("BPM " + str(tempo) + '\n')
-    file.write("PAGE_SHIFT " + offset + '\n')
+    file.write("BPM " + str(tempo * 2) + '\n')
+    file.write("PAGE_SHIFT " + offset/1000 + '\n')
     file.write("PAGE_SIZE " + 240/float(offset) + '\n')
     for count, item in beat_list:
         file.write("NOTE\t" + str(count) + '\t' + str(round(item, 6)) + '\t' + str(x_coords) + '\t' + "0.000000" + '\n')
