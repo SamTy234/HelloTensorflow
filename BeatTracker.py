9e667b73d29ded35f614d48275628f66e0fefefd
Samuel Gasper <1516828@my.brunel.ac.uk>
2019-03-18 04:04:36 +0000

    Made offset first known beat in beat tracker

--- BeatTracker.py
+++ BeatTracker.py
@@ -9,11 +9,11 @@ y_timings = []
 x_coords = round(random.uniform(0.0, 1.0), 6)
 
 
-y, sr = librosa.load("easy-song-1.wav")
+y, sr = librosa.load("SS2.mp3")
 
 
 BPM = input("Enter BPM: ")
-offset = input("Enter offset: ")
+
 
 # Beat tracker
 tempo, beats = librosa.beat.beat_track(y=y, sr=sr, start_bpm=BPM)
@@ -30,8 +30,8 @@ beat_list = list(enumerate(beat_times))
 with open("testfile.txt", "w+") as file:
     file.write("VERSION 2\n")
     file.write("BPM " + str(tempo * 2) + '\n')
-    file.write("PAGE_SHIFT " + offset/1000 + '\n')
-    file.write("PAGE_SIZE " + 240/float(offset) + '\n')
+    file.write("PAGE_SHIFT " + beat_times[0] + '\n')
+    file.write("PAGE_SIZE " + 240/float(BPM) + '\n')
     for count, item in beat_list:
         file.write("NOTE\t" + str(count) + '\t' + str(round(item, 6)) + '\t' + str(x_coords) + '\t' + "0.000000" + '\n')
 
