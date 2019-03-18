e82c88b7be173b8124c44ed05bb7268d8c0416de
Samuel Gasper <1516828@my.brunel.ac.uk>
2019-03-18 15:32:32 +0000

    Add files via upload
    Tweaked it so it works as intended with creating taps

--- BeatTracker.py
+++ BeatTracker.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 23:08:30 2019
+
+@author: Sam Tyson Gasper
+"""
+
+
 import librosa
 import random
 
@@ -9,14 +17,14 @@ y_timings = []
 x_coords = round(random.uniform(0.0, 1.0), 6)
 
 
-y, sr = librosa.load("SS2.mp3")
+y, sr = librosa.load(r"S:/Charts/Rigid/Song files/easy/MP3 files/SS2.mp3")
 
 
 BPM = input("Enter BPM: ")
 
 
 # Beat tracker
-tempo, beats = librosa.beat.beat_track(y=y, sr=sr, start_bpm=BPM)
+tempo, beats = librosa.beat.beat_track(y=y, sr=sr, start_bpm=int(BPM))
 
 
 #Convert beattracker frames to seconds
@@ -29,10 +37,10 @@ beat_list = list(enumerate(beat_times))
 
 with open("testfile.txt", "w+") as file:
     file.write("VERSION 2\n")
-    file.write("BPM " + str(tempo * 2) + '\n')
-    file.write("PAGE_SHIFT " + beat_times[0] + '\n')
-    file.write("PAGE_SIZE " + 240/float(BPM) + '\n')
+    file.write("BPM " + str(round((tempo * 2), 6)) + '\n')
+    file.write("PAGE_SHIFT " + str(round(beat_times[0], 6)) + '\n')
+    file.write("PAGE_SIZE " + str(240/float(BPM)) + '\n')
     for count, item in beat_list:
-        file.write("NOTE\t" + str(count) + '\t' + str(round(item, 6)) + '\t' + str(x_coords) + '\t' + "0.000000" + '\n')
+        file.write("NOTE\t" + str(count) + '\t' + str(round(item, 6)) + '\t' + str(round(random.uniform(0.0, 1.0), 6)) + '\t' + "0.000000" + '\n')
 
     file.close()
