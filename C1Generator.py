a0b5bbceece9ae0f19d40075f542df0729c9004c
Samuel Gasper <1516828@my.brunel.ac.uk>
2019-03-28 01:41:15 +0000

    Removed hold note generation and added user input for the shift

--- C1Generator.py
+++ C1Generator.py
@@ -30,8 +30,21 @@ def save_filename():
 
 
 def get_bpm():
-    text = bpm_entry.get()
-    return text
+    bpm = bpm_entry.get()
+    string_display = "Your estimated BPM is " + bpm
+    label = Label(root)
+    label["text"] = string_display
+    label.grid(row=3, column=1)
+    return bpm
+
+
+def get_shift():
+    shift = shift_entry.get()
+    string_display = "Your current shift: " + shift
+    label = Label(root)
+    label["text"] = string_display
+    label.grid(row=4, column=1)
+    return shift
 
 
 def calculate_bpm():
@@ -40,7 +53,7 @@ def calculate_bpm():
 
 
 def calculate_shift():
-    return round(beat_times_percussive[0], 6)
+    return round(onset_times[0], 6)
 
 
 def random_x_generator():
@@ -58,25 +71,42 @@ def determine_holds(item):
         return "0.000000"
 
 
+
+def choose_between_auto_shift_or_user_shift():
+    user_shift = get_shift()
+    if user_shift is not None:
+        return user_shift
+    else:
+        return calculate_shift()
+
 def get_c1_file():
-    global tempo, beat_times_percussive, beat_times_harmonic
+    global tempo, beat_times_percussive, beat_times_harmonic, onset_times
     # Load the file and separate into harmonics/Percussives
     y, sr = librosa.load(browse_file())
-    Label(root, text="File loaded successfully").grid(row=2, column=1)
-    Label(root, text="Separating Harmonics...").grid(row=3, column=1)
+    #Label(root, text="File loaded successfully").grid(row=2, column=1)
+    print("File loaded successfully")
+    #Label(root, text="Separating Harmonics...").grid(row=3, column=1)
+    print("Seperating Harmonic beats..")
 
     y_harmonic = librosa.effects.harmonic(y)
-    Label(root, text="Separating Percussives...").grid(row=4, column=1)
+    # Label(root, text="Separating Percussives...").grid(row=4, column=1)
+    print("Seperating percussives..")
 
     y_percussive = librosa.effects.percussive(y)
-    Label(root, text="Getting BPM...").grid(row=5, column=1)
+    # Label(root, text="Getting BPM...").grid(row=5, column=1)
+    print("Getting BPM...")
+
     estimated_bpm = get_bpm()
     tempo, beats = librosa.beat.beat_track(y=y, sr=sr, start_bpm=int(estimated_bpm))
 
     # Convert beat tracker frames to seconds
-    Label(root, text="Generating Beat times...").grid(row=6, column=1)
+    # Label(root, text="Generating Beat times...").grid(row=6, column=1)
+    print("Generating Beat times...")
+
     beat_times = librosa.frames_to_time(beats, sr=sr)
     beat_list = list(enumerate(beat_times))
+    print("Beat times sucessfully converted to a list")
+
 
     # Onset tracker
     onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
@@ -90,15 +120,15 @@ def get_c1_file():
     # Convert beattracker harmonic
     tempo, beat_frames_harmonic = librosa.beat.beat_track(y=y_harmonic, sr=sr)
     beat_times_harmonic = librosa.frames_to_time(beat_frames_harmonic, sr=sr)
-    
+
     with open(save_filename(), "w+") as file:
         file.write("VERSION 2\n")
         file.write("BPM " + str(calculate_bpm()) + '\n')
-        file.write("PAGE_SHIFT " + str(calculate_shift()) + '\n')
+        file.write("PAGE_SHIFT " + str(choose_between_auto_shift_or_user_shift()) + '\n')
         file.write("PAGE_SIZE " + str(calculate_page_size()) + '\n')
         for count, item in beat_list:
             file.write("NOTE\t" + str(count) + '\t' + str(round(item, 6)) + '\t' + str(
-                random_x_generator()) + '\t' + str(determine_holds(item)) + '\n')
+                random_x_generator()) + '\t' + '0.000000' + '\n')
 
         print("Chart done!")
 
@@ -109,23 +139,27 @@ root = Tk()
 menubar = Menu(root)
 
 root.config(menu=menubar)
-root.geometry('350x350')
+root.geometry('350x150')
 
 root.title('C1 Generator')
 
 subMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="File", menu=subMenu)
-subMenu.add_command(label="Open", command=browse_file)
-
+#subMenu.add_command(label="Open", command=browse_file)
 subMenu.add_command(label="Exit", command=root.destroy)
+
 bpm_label = Label(root, text="Estimated BPM:").grid(row=0, column=0)
 bpm_entry = Entry(root, bd=2)
-
 bpm_entry.grid(row=0, column=1)
 register_bpm_button = Button(root, text="Enter", command=lambda: get_bpm()).grid(row=0, column=2)
-output_text_button = Button(root, text="Get your C1 file", command=lambda: get_c1_file())
 
-output_text_button.grid(row=1, column=1)
+shift_label = Label(root, text="Shift Timestamp:").grid(row=1, column=0)
+shift_entry = Entry(root, bd=2)
+shift_entry.grid(row=1, column=1)
+register_shift_button = Button(root, text="Enter", command=lambda: get_shift()).grid(row=1, column=2)
+
+output_text_button = Button(root, text="Get your C1 file", command=lambda: get_c1_file())
+output_text_button.grid(row=2, column=1)
 
 
 root.mainloop()
